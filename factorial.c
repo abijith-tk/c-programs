eab70f86a7987ae50814cc7ea9c3c983bb12165b
Abijith T K <88266300+abijith-tk@users.noreply.github.com>
2022-09-14 15:44:43 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+// factorial
+
+int main()
+{
+ int number,fact=1;
+ printf("Number=");
+ scanf("%d",&number);
+ for(int i=1;i<number;i++)
+ {
+  fact=fact*i;
+ }
+ printf("Factorial=%d\n",fact);
+ return 0;
+}
