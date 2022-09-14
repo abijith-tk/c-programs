50cf3c568d3303fd67c8785613d3556e2356c930
Abijith T K <88266300+abijith-tk@users.noreply.github.com>
2022-09-14 15:26:42 +0530

    Add files via upload

---
+++ sum_upto_a_number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// sum from 1 to a given number
+
+int main()
+{
+ int number,sum;
+ printf("Number=");
+ scanf("%d",&number);
+ sum=(number*(number+1))/2;
+ printf("Sum upto %d=%d\n",number,sum);
+ return 0;
+}
