872dc09e74f418a4c2d3d298250adf679291b982
Abijith T K <88266300+abijith-tk@users.noreply.github.com>
2022-09-14 14:56:58 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+// read two numbers and return their product if it is less than 1000 else their sum
+
+int main()
+{
+ int num1,num2,sum,product;
+ printf("Num1=");
+ scanf("%d",&num1);
+ printf("Num2=");
+ scanf("%d",&num2);
+ product=num1*num2;
+ if(product<1000)
+ {
+  printf("Product=%d",product);
+ }
+ else 
+ {
+  sum=num1+num2;
+  printf("Sum=%d",sum);
+ }
+ return 0;
+}
