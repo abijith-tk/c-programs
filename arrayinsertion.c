bc53eb1f9cc07efc412834a69467acfdc7a38c64
abijith T K <88266300+abijith-tk@users.noreply.github.com>
2022-09-18 18:31:22 +0530

    comments are added

--- arrayinsertion.c
+++ arrayinsertion.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+// progarm to insert elements to an array
 int main()
 {
     int array[50];
     int size,pos,ele,temp;
     printf("Enter the size of the array:");
     scanf("%d",&size);
+    
+    // reading elements to the array
     printf("Enter the array elements\n");
     for(int i=0; i<size; i++)
     {
@@ -22,6 +24,8 @@ int main()
     scanf("%d",&ele);
     printf("Enter the position:");
     scanf("%d",&pos);
+    
+    // swapping elements to place new element at the position
     if(pos>=0||pos<size)
     {
         for(int i=size-1; i>pos; i--)
