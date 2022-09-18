4508d12d08caaba804e761aa8814c651a9b2ff1d
abijith T K <88266300+abijith-tk@users.noreply.github.com>
2022-09-18 18:34:00 +0530

    Update Fibonacci.c

--- Fibonacci.c
+++ Fibonacci.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+// program to display fibonacci series
 int main()
 {
-  int a=-1,b=1,c;
-  for(int i=1;i<=10;i++)
+  int a=-1,b=1,c,n;
+  printf("Enter n:");
+  scanf("%d",&n);
+  printf("Fibonacci series upto n numbers\n");
+  for(int i=1;i<=n;i++)
   {
     c=a+b;
     printf("%d\t",c);
