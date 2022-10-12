4e2051f73f98450d2bcae1d5f07a70b1f7c965d4
abijith tk <88266300+abijith-tk@users.noreply.github.com>
2022-10-12 17:32:32 +0530

    Add files via upload

--- linearsearch.c
+++ linearsearch.c
@@ -1,27 +1,35 @@
 #include<stdio.h>
+
+// linear search : searching begins from 0th index and searches 
+// it seqenetially until the element is found or end of the array 
+
 int main()
 {
- int array[50],size,i,ele,flag=0;
+ int array[50],size,ele,flag=0;
  printf("Enter the size of the array: ");
  scanf("%d",&size);
  printf("Enter array elements:\n");
- for(i=0;i<size;i++)
+ for(int i=0;i<size;i++)
  {
+  printf("array[%d]=",i);
   scanf("%d",&array[i]);
  }
  printf("Enter the element to search: ");
  scanf("%d",&ele);
- for(i=0;i<size;i++)
+ for(int i=0;i<size;i++)
  {
-  if(ele==array[i])
+  if(array[i]==ele)
   {
    printf("%d found at array[%d]\n",ele,i);
    flag=1;
    break;
   }
  }
- if(flag==0)
+ 
+ // flag=0 ,if the elemet is not present in the arrays
+ if(flag!=1)
  {
   printf("Element not found\n");
  } 
+ return 0;
 }
