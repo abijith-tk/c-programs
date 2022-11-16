2a8af3ee8c38d859b70eeaa1e5956bceadd8ba7e
abijith tk <88266300+abijith-tk@users.noreply.github.com>
2022-11-16 13:51:54 +0530

    Create bubble_sort.c

---
+++ bubble_sort.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+        int size;
+
+        printf("Size=");
+
+        scanf("%d",&size);
+
+        int arr[size];
+
+        printf("Enter array elements :");
+
+        for(int i=0;i<size;i++)
+
+        {
+
+                scanf("%d",&arr[i]);
+
+        }
+
+        for(int i=0;i<size;i++)
+
+        {
+
+                for(int j=0;j<size-1;j++)
+
+                {
+
+                        if(arr[j]>arr[j+1])
+
+                        {
+
+                                int temp;
+
+                                temp=arr[j];
+
+                                arr[j]=arr[j+1];
+
+                                arr[j+1]=temp;
+
+                        }
+
+                }
+
+        }
+
+        printf("Sorted array\n");
+
+        for(int i=0;i<size;i++)
+
+        {
+
+                printf("%d\t",arr[i]);
+
+        }
+
+        return 0;
+
+}
