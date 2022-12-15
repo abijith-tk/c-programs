0b956300f1d3fb7402f08ce183a2857252c8876c
abijith tk <88266300+abijith-tk@users.noreply.github.com>
2022-12-15 15:35:02 +0530

    comments added for better understanding

--- fact.c
+++ fact.c
@@ -3,44 +3,41 @@
 int isfact(int n)
 {
  int i=2;
- int k=1;
+ int k=1;             
  while(n!=1 && n%i==0)
  {
   n=n/i;
-  k++;
+  k++;              
   i++;
  }
  if(n==1)
  {
-  printf("%d",k);
+  printf("%d",k);       // k!=n
  }
  else{
   printf("Not a factorial");
  }
 }
 
-//int main(int argc, char *argv[])
+//int main(int argc, char *argv[])   // command line argument
 int main()
 {
- //isfact(atoi(argv[1]));
+ //isfact(atoi(argv[1]));           // command line argument
  isfact(120);
  return 0;
 }
 /*
-
-n=120,i=2
-
-120/2=60   n/i
-           n=n/i  , i++
-60/3=20
-
-20/4=5
-
-5/5=1
+rough work.......
+n=120,i=2             k=1
+120/2=60   n/i ,      k=2
+60/3=20    (n/i)/i++, k=3
+20/4=5                k=4
+5/5=1                 k=5
 
 7/2=3
 3/3=1
 
+n=24,k=1
 24/2--12/3--4/4
-
+k=2--k=3--k=4
 */
