2a734f97d0f1f2d2fdf42d1baae0b2a5b4a5b690
abijith tk <88266300+abijith-tk@users.noreply.github.com>
2022-12-15 16:28:37 +0530

    comments added
    Explained Adam number

--- adamnum.c
+++ adamnum.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
+// program to find adam numbers upto a numbers
 
+/*       
+             square
+ number   12 = 144 
+ reverse  21 = 441  
+ 
+ reverse of number^2=reverse^2 ,then n is an Adam number
+*/
+
+// function to find the reverse(n)
 int rev(int n)
 {
 	int reverse=0;
- while(n>0)
- {
- 	reverse=reverse*10+(n%10);
- 	n=n/10;
- }
- return reverse;
+        while(n>0)
+        {
+ 		reverse=reverse*10+(n%10);
+ 		n=n/10;
+ 	}
+ 	return reverse;
 }
 
 int main()
